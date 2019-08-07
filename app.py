1f21a608675b164a79786b4fe828dad3153574a2
Dave Bland <david.bland@gmx.co.uk>
2019-08-07 12:05:33 +0100

    form handling and start on sessions

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "arandomstring")
 
 """ Route handler functions """
-
-
 
 """ Routes """
 
 @app.route('/', methods=["GET", "POST"])
-def login():
+def home():
     if request.method == "POST":
-        return request.form
+        return redirect(url_for('login'), code=307)    
     return render_template('login.html')
+
+@app.route('/login', methods=["POST"])
+def login():
+    if 'login-email' in request.form:
+        # Set email as user_id in session
+        session["user_id"] = request.form['login-email']
+        return "SESSION USER ID:" + session["user_id"]
+    return request.form
 
 @app.route('/home')
 def temp():
