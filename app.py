579ff319c3502780dc2e71a3c3ef4c8c7d0f1e69
Dave Bland <david.bland@gmx.co.uk>
2019-08-10 20:25:41 +0100

    refactored routes and added table data and flash messages to home

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import os
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "arandomstring")
 
-""" Route handler functions """
+# ROUTE HELPER FUNCTIONS
 
 def login_check():
-    # If user logged in return true otherwise false
+    """ If user logged in return true otherwise false """
     if 'user_id' in session:
         return True
     return False
 
-""" Routes """
+# ROUTES
 
 @app.route('/')
 def index():
+    """ Main route returning app homepage or else login page """
     if login_check():
-        return render_template('logbook-home.html', title="Logbook Home")
+        # Get record set for user if any exist, otherwise return no record flash
+        record_set = "RECORDS FROM DB"
+        if record_set:
+            return render_template('logbook-home.html', title="Logbook Home", user_records=record_set)
+        else:
+            flash('No records found')
+            return render_template('logbook-home.html', title="Logbook Home")
+
     return render_template('login.html')
 
 @app.route('/login', methods=["POST"])
 def login():
+    """ Login a user by setting session and redirect to home """
     if 'login-email' in request.form:
         # Set email as user_id in session
         session["user_id"] = request.form['login-email']
@@ -34,28 +43,46 @@ def login():
 
 @app.route('/logout')
 def logout():
-    # Destroy session and return to home
+    """" Logout a user by destroying session and redirect to home """
     session.clear()
     return redirect(url_for('index'))
 
+@app.route('/get/<record_id>')
+def get(record_id):
+    """ Get a record and return all fields """
+    if login_check():
+        return "YOUR REQUESTED: %s" % record_id
+    return redirect( url_for('index') )
+
 @app.route('/new')
 def new():
-    """ Generate page for new entry """
+    """ Generate page for entry of a new record """
     if login_check():
-        return render_template('update.html', title="New Entry")
+        return render_template('new-edit.html', title="New record")
     return redirect( url_for('index') )
 
-@app.route('/update/<entry_id>')
-def update(entry_id):
-    """ Generate page to update existing entry """
+@app.route('/edit/<record_id>')
+def edit(record_id):
+    """ Generate page to edit an existing record """
     if login_check():
-        return render_template('update.html', title="Edit Entry")
+        return render_template('new-edit.html', title="Edit record", record=record_id)
     return redirect( url_for('index') )
 
-@app.route('/get/<entry_id>')
-def get(entry_id):
-    """ Return all fields of record """
-    return "{'test' : %s }" % entry_id
+@app.route('/update/<record_id>', methods=["POST"])
+def update(record_id):
+    """ Update an existing record in DB """
+    if login_check():
+        # Connect to DB and update record
+        return "DB CONNECT HERE FOR UPDATE"
+    return redirect( url_for('index') )
+
+@app.route('/delete/<record_id>', methods=["POST"])
+def delete(record_id):
+    """ Delete an existing record in DB """
+    if login_check():
+        # Connect to DB and delete record
+        return "DB CONNECT HERE FOR DELETE"
+    return redirect( url_for('index') )
 
 # Set flask parameters
 if __name__ == '__main__':
